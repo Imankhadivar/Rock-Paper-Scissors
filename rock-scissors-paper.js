aa7a01ed76022b2dc350a980dddd04ce2cc06310
Imankhadivar <khadivarimanr@gmail.com>
2023-02-05 15:18:51 -0800

    completed_RockScissorsPaper_DOM_version

--- rock-scissors-paper.js
+++ rock-scissors-paper.js
@@ -3,11 +3,10 @@ const possible_choices = ["rock", "scissors", "paper"]
 function getComputerChoice (){
     const random = Math.floor(Math.random()*possible_choices.length);
     let computerChoice = possible_choices[random];
-    return computerChoice;
-    
+    return computerChoice;    
 }
-let playerInput = prompt("Please enter your choice");  
-let playerSelection = playerInput.toLowerCase();
+// let playerInput = prompt("Please enter your choice");  
+// let playerSelection = playerInput.toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
     if (!possible_choices.includes(playerSelection)) {
@@ -23,7 +22,7 @@ function playRound(playerSelection, computerSelection) {
         return "You win! Paper beats Rock"
     }
     else if (playerSelection === "scissors" && computerSelection === "rock"){
-        return "You lose! Scissors beats Rock"
+        return "You lose! Rock beats scissors"
     }
     else if (playerSelection === "rock" && computerSelection === "paper"){
         return "You lose! Paper beats Rock"
@@ -35,9 +34,26 @@ function playRound(playerSelection, computerSelection) {
         return "It's a tie!"
     }
 }
+const body = document.querySelector('body');
+const div = document.createElement('div');
+body.appendChild(div);
+const btnRock = document.querySelector('#btn-rock');
+btnRock.addEventListener('click', () => {
+    div.textContent = '';
+    div.textContent = playRound('rock', getComputerChoice())
+})
+const btnScissors = document.querySelector('#btn-scissors');
+btnScissors.addEventListener('click', () => {
+    div.textContent = '';
+    div.textContent = playRound('scissors', getComputerChoice())
+})
+const btnPaper = document.querySelector('#btn-paper');
+btnPaper.addEventListener('click', () => {
+    div.textContent = '';
+    div.textContent = playRound('paper', getComputerChoice())
+})
 
   
-  
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
- 
+const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+    
